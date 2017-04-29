14b44ec85a2862a3a6217757b177529c60cc58da
Bruno Lima <castanho.bruno@gmail.com>
2017-04-29 16:35:39 -0300

    Fixing the initial setup

--- App/PluginLoader.cs
+++ App/PluginLoader.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Advanced_Combat_Tracker;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FFXIVPostParse
 {
-    public class PluginLoader
+    public class PluginLoader : IActPluginV1
     {
+        public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeInitPlugin()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
