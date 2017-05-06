2f048eac12a3afa7b2dc6a2fdfd9ab145e7487fc
Bruno Lima <castanho.bruno@gmail.com>
2017-05-06 14:28:14 -0300

    Adding the model for the future json serialization

---
+++ App/Model/Log.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVPostParse.Model
+{
+    class Log
+    {
+        public String StartTime { get; set; }
+        public String Duration { get; set; }
+        public String TotalHps { get; set; }
+        public String BossName { get; set; }
+        public String MapName { get; set; }
+        public Boolean IncludeHps { get; set; }
+        public List<Player> Players { get; set; }
+
+        public Log()
+        {
+            this.StartTime = "";
+            this.Duration = "";
+            this.TotalHps = "";
+            this.BossName = "";
+            this.MapName = "";
+            this.IncludeHps = true;
+            this.Players = new List<Player>();
+        }
+    }
+}
---
+++ App/Model/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVPostParse.Model
+{
+    class Player
+    {
+        public String PlayerJob { get; set; }
+        public String PlayerName { get; set; }
+        public String DamagePercentage { get; set; }
+        public String Dps { get; set; }
+        public String HealingPercentage { get; set; }
+        public String Hps { get; set; }
+        public String Deaths { get; set; }
+        public String Crits { get; set; }
+        public String Misses { get; set; }
+        public Boolean JobIconEnabled { get; set; }
+
+        public Player()
+        {
+            this.PlayerJob = "";
+            this.PlayerName = "";
+            this.DamagePercentage = "";
+            this.Dps = "";
+            this.HealingPercentage = "";
+            this.Hps = "";
+            this.Deaths = "";
+            this.Crits = "";
+            this.Misses = "";
+            this.JobIconEnabled = true;
+        }
+    }
+}
