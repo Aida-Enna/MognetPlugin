e014beb9f524eca2a9a63fdbb96b872bcf2723dd
Bruno Lima <castanho.bruno@gmail.com>
2017-05-07 18:03:17 -0300

    Modifiyng the plugin screen. Added components (Attributes and Log)

--- App/Control/PluginControl.Designer.cs
+++ App/Control/PluginControl.Designer.cs
@@ -1,6 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using FFXIVPostParse.Enum;
+using System.Windows.Forms;
 
-namespace FFXIVPostParse
+namespace FFXIVPostParse.Control
 {
     partial class PluginControl
     {
@@ -21,12 +22,16 @@ namespace FFXIVPostParse
             }
             base.Dispose(disposing);
         }
-
-        private GroupBox groupStatus;
-        private Label combatStatus;
-        private GroupBox groupOptions;
-        private CheckBox enabled;
-        private CheckBox includeHPS;
+        private GroupBox grpOptions;
+        private Label lblPlayerName;
+        private TextBox txtPlayerName;
+        private Button btnApplyName;
+        private CheckBox chkEnabled;
+        private GroupBox grpHttpLog;
+        private RichTextBox rchPluginLog;
+        private Button btnClearLog;
+        private GroupBox grpAttributes;
+        private CheckedListBox chlAttributes;
 
         #region Windows Form Designer generated code
 
@@ -36,75 +41,156 @@ namespace FFXIVPostParse
         /// </summary>
         private void InitializeComponent()
         {
-            this.combatStatus = new System.Windows.Forms.Label();
-            this.enabled = new System.Windows.Forms.CheckBox();
-            this.includeHPS = new System.Windows.Forms.CheckBox();
-            this.groupStatus = new System.Windows.Forms.GroupBox();
-            this.groupOptions = new System.Windows.Forms.GroupBox();
-            this.groupStatus.SuspendLayout();
-            this.groupOptions.SuspendLayout();
+            this.chkEnabled = new System.Windows.Forms.CheckBox();
+            this.grpOptions = new System.Windows.Forms.GroupBox();
+            this.lblPlayerName = new System.Windows.Forms.Label();
+            this.txtPlayerName = new System.Windows.Forms.TextBox();
+            this.btnApplyName = new System.Windows.Forms.Button();
+            this.grpHttpLog = new System.Windows.Forms.GroupBox();
+            this.rchPluginLog = new System.Windows.Forms.RichTextBox();
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.grpAttributes = new System.Windows.Forms.GroupBox();
+            this.chlAttributes = new System.Windows.Forms.CheckedListBox();
+            this.grpOptions.SuspendLayout();
+            this.grpHttpLog.SuspendLayout();
+            this.grpAttributes.SuspendLayout();
             this.SuspendLayout();
             // 
-            // combatStatus
+            // chkEnabled
             // 
-            this.combatStatus.Location = new System.Drawing.Point(6, 17);
-            this.combatStatus.Name = "combatStatus";
-            this.combatStatus.Size = new System.Drawing.Size(252, 20);
-            this.combatStatus.TabIndex = 0;
-            this.combatStatus.Text = "Waiting a fight...";
+            this.chkEnabled.Checked = true;
+            this.chkEnabled.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkEnabled.Location = new System.Drawing.Point(6, 53);
+            this.chkEnabled.Name = "chkEnabled";
+            this.chkEnabled.Size = new System.Drawing.Size(249, 24);
+            this.chkEnabled.TabIndex = 0;
+            this.chkEnabled.Text = "Check this to enable the plugin";
+            this.chkEnabled.CheckedChanged += new System.EventHandler(this.chkEnabled_CheckedChanged);
             // 
-            // enabled
+            // grpOptions
             // 
-            this.enabled.Checked = true;
-            this.enabled.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.enabled.Location = new System.Drawing.Point(6, 23);
-            this.enabled.Name = "enabled";
-            this.enabled.Size = new System.Drawing.Size(249, 24);
-            this.enabled.TabIndex = 0;
-            this.enabled.Text = "Check this to enable the plugin";
-            this.enabled.CheckedChanged += new System.EventHandler(this.enabled_CheckedChanged);
+            this.grpOptions.Controls.Add(this.lblPlayerName);
+            this.grpOptions.Controls.Add(this.txtPlayerName);
+            this.grpOptions.Controls.Add(this.btnApplyName);
+            this.grpOptions.Controls.Add(this.chkEnabled);
+            this.grpOptions.Location = new System.Drawing.Point(9, 3);
+            this.grpOptions.Name = "grpOptions";
+            this.grpOptions.Size = new System.Drawing.Size(430, 86);
+            this.grpOptions.TabIndex = 0;
+            this.grpOptions.TabStop = false;
+            this.grpOptions.Text = "Options";
             // 
-            // includeHPS
+            // lblPlayerName
             // 
-            this.includeHPS.Checked = true;
-            this.includeHPS.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.includeHPS.Location = new System.Drawing.Point(6, 53);
-            this.includeHPS.Name = "includeHPS";
-            this.includeHPS.Size = new System.Drawing.Size(249, 24);
-            this.includeHPS.TabIndex = 1;
-            this.includeHPS.Text = "Check this to include Healing per Second";
+            this.lblPlayerName.Location = new System.Drawing.Point(6, 30);
+            this.lblPlayerName.Name = "lblPlayerName";
+            this.lblPlayerName.Size = new System.Drawing.Size(85, 20);
+            this.lblPlayerName.TabIndex = 2;
+            this.lblPlayerName.Text = "Your log name:";
             // 
-            // groupStatus
+            // txtPlayerName
             // 
-            this.groupStatus.Controls.Add(this.combatStatus);
-            this.groupStatus.Location = new System.Drawing.Point(0, 0);
-            this.groupStatus.Name = "groupStatus";
-            this.groupStatus.Size = new System.Drawing.Size(264, 40);
-            this.groupStatus.TabIndex = 0;
-            this.groupStatus.TabStop = false;
-            this.groupStatus.Text = "Status";
+            this.txtPlayerName.Location = new System.Drawing.Point(97, 27);
+            this.txtPlayerName.MaxLength = 20;
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new System.Drawing.Size(187, 20);
+            this.txtPlayerName.TabIndex = 3;
+            this.txtPlayerName.Text = "YOU";
             // 
-            // groupOptions
+            // btnApplyName
             // 
-            this.groupOptions.Controls.Add(this.enabled);
-            this.groupOptions.Controls.Add(this.includeHPS);
-            this.groupOptions.Location = new System.Drawing.Point(3, 46);
-            this.groupOptions.Name = "groupOptions";
-            this.groupOptions.Size = new System.Drawing.Size(261, 91);
-            this.groupOptions.TabIndex = 0;
-            this.groupOptions.TabStop = false;
-            this.groupOptions.Text = "Options";
+            this.btnApplyName.Location = new System.Drawing.Point(290, 25);
+            this.btnApplyName.Name = "btnApplyName";
+            this.btnApplyName.Size = new System.Drawing.Size(75, 23);
+            this.btnApplyName.TabIndex = 4;
+            this.btnApplyName.Text = "Apply";
+            this.btnApplyName.Click += new System.EventHandler(this.btnApplyName_Click);
+            // 
+            // grpHttpLog
+            // 
+            this.grpHttpLog.Controls.Add(this.rchPluginLog);
+            this.grpHttpLog.Controls.Add(this.btnClearLog);
+            this.grpHttpLog.Location = new System.Drawing.Point(3, 95);
+            this.grpHttpLog.Name = "grpHttpLog";
+            this.grpHttpLog.Size = new System.Drawing.Size(430, 240);
+            this.grpHttpLog.TabIndex = 1;
+            this.grpHttpLog.TabStop = false;
+            this.grpHttpLog.Text = "Log";
+            // 
+            // rchPluginLog
+            // 
+            this.rchPluginLog.Location = new System.Drawing.Point(6, 19);
+            this.rchPluginLog.Name = "rchPluginLog";
+            this.rchPluginLog.ReadOnly = true;
+            this.rchPluginLog.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.rchPluginLog.Size = new System.Drawing.Size(418, 177);
+            this.rchPluginLog.TabIndex = 1;
+            this.rchPluginLog.Text = "";
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Location = new System.Drawing.Point(6, 205);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(75, 23);
+            this.btnClearLog.TabIndex = 2;
+            this.btnClearLog.Text = "Clear Log";
+            this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click);
+            // 
+            // grpAttributes
+            // 
+            this.grpAttributes.Controls.Add(this.chlAttributes);
+            this.grpAttributes.Location = new System.Drawing.Point(445, 3);
+            this.grpAttributes.Name = "grpAttributes";
+            this.grpAttributes.Size = new System.Drawing.Size(178, 332);
+            this.grpAttributes.TabIndex = 2;
+            this.grpAttributes.TabStop = false;
+            this.grpAttributes.Text = "Include Attributes";
+            // 
+            // chlAttributes
+            // 
+            this.chlAttributes.CheckOnClick = true;
+            this.chlAttributes.Items.AddRange(new object[] {
+            AttributeEnum.START_TIME,
+            AttributeEnum.DURATION,
+            AttributeEnum.MAX_HIT_PARTY,
+            AttributeEnum.TOTAL_HEALING,
+            AttributeEnum.MAP_NAME,
+            AttributeEnum.BOSS_NAME,
+            AttributeEnum.PLAYER_JOB,
+            AttributeEnum.PLAYER_NAME,
+            AttributeEnum.DPS,
+            AttributeEnum.DAMAGE_PERCENTAGE,
+            AttributeEnum.MAX_HIT_INDIVIDUAL,
+            AttributeEnum.HPS,
+            AttributeEnum.HEALING_PERCENTAGE,
+            AttributeEnum.MAX_HEAL,
+            AttributeEnum.OVERHEAL_PERCENTAGE,
+            AttributeEnum.DEATHS,
+            AttributeEnum.CRITS,
+            AttributeEnum.MISEES});
+            this.chlAttributes.SetItemChecked(4, true);
+            this.chlAttributes.SetItemChecked(5, true);
+            this.chlAttributes.SetItemChecked(6, true);
+            this.chlAttributes.SetItemChecked(7, true);
+            this.chlAttributes.SetItemChecked(8, true);
+            this.chlAttributes.Location = new System.Drawing.Point(6, 16);
+            this.chlAttributes.Name = "chlAttributes";
+            this.chlAttributes.Size = new System.Drawing.Size(164, 304);
+            this.chlAttributes.TabIndex = 1;
             // 
             // PluginControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupStatus);
-            this.Controls.Add(this.groupOptions);
+            this.Controls.Add(this.grpOptions);
+            this.Controls.Add(this.grpHttpLog);
+            this.Controls.Add(this.grpAttributes);
             this.Name = "PluginControl";
-            this.Size = new System.Drawing.Size(479, 266);
-            this.groupStatus.ResumeLayout(false);
-            this.groupOptions.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(643, 348);
+            this.grpOptions.ResumeLayout(false);
+            this.grpOptions.PerformLayout();
+            this.grpHttpLog.ResumeLayout(false);
+            this.grpAttributes.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
--- App/Control/PluginControl.cs
+++ App/Control/PluginControl.cs
@@ -1,14 +1,10 @@
-﻿using System;
+﻿using FFXIVPostParse.Enum;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FFXIVPostParse
+namespace FFXIVPostParse.Control
 {
     public partial class PluginControl : UserControl
     {
@@ -17,23 +13,67 @@ namespace FFXIVPostParse
         public PluginControl()
         {
             InitializeComponent();
+            this.IsEnabled = true;
         }
 
-        private void enabled_CheckedChanged(object sender, EventArgs e)
+        private void chkEnabled_CheckedChanged(object sender, EventArgs e)
         {
-            if (this.enabled.Checked)
+            if (this.chkEnabled.Checked)
             {
                 this.IsEnabled = true;
+                AddPluginLogText("Plugin enabled. Log will be sent after the ending of an encounter.");
             }
             else
             {
+                AddPluginLogText("Plugin disabled. No log will be sent.");
                 this.IsEnabled = false;
             }
         }
 
-        public void CombatStatusText(String text)
+        public void AddPluginLogText(String text)
         {
-            this.combatStatus.Text = text;
+            text = DateTime.Now.ToString("HH:mm:ss") + ": " + text;
+            text += Environment.NewLine;
+            this.rchPluginLog.Text += text;
+        }
+
+        public String GetPlayerName()
+        {
+            return this.txtPlayerName.Text;
+        }
+
+        public List<AttributeEnum> GetCheckedAttributes()
+        {
+            List<AttributeEnum> attributes = new List<AttributeEnum>();
+            IEnumerator e = this.chlAttributes.CheckedIndices.GetEnumerator();
+            while (e.MoveNext())
+            {
+                AttributeEnum attrib = AttributeEnum.GetAttributeByIndex((int) e.Current);
+                if (attrib != null)
+                {
+                    attributes.Add(attrib);
+                }
+                
+            }
+
+            return attributes;
+        }
+
+        private void btnClearLog_Click(object sender, EventArgs e)
+        {
+            this.rchPluginLog.Clear();
+        }
+
+        private void btnApplyName_Click(object sender, EventArgs e)
+        {
+            String playerName = txtPlayerName.Text;
+            playerName = playerName.TrimStart(' ');
+            playerName = playerName.TrimEnd(' ');
+            if (playerName == "")
+            {
+                playerName = "YOU";
+            }
+            txtPlayerName.Text = playerName;
         }
     }
 
