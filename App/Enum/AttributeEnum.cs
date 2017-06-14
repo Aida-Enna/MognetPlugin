4551a98ece545fcf888601a9fc82a8045e8a042a
Bruno Lima <castanho.bruno@gmail.com>
2017-06-14 06:21:10 -0300

    Refactor the enum and create SuccessLevel

--- App/Enum/AttributeEnum.cs
+++ App/Enum/AttributeEnum.cs
@@ -1,103 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FFXIVPostParse.Enum
+﻿namespace MognetPlugin.Enum
 {
     public class AttributeEnum
     {
-        public static readonly AttributeEnum START_TIME = new AttributeEnum(0, "Start Time");
-        public static readonly AttributeEnum DURATION = new AttributeEnum(1, "Duration");
-        public static readonly AttributeEnum MAX_HIT_PARTY = new AttributeEnum(2, "Max Hit (Party)");
-        public static readonly AttributeEnum TOTAL_HEALING = new AttributeEnum(3, "Total Healing");
-        public static readonly AttributeEnum MAP_NAME = new AttributeEnum(4, "Map Name");
-        public static readonly AttributeEnum BOSS_NAME = new AttributeEnum(5, "Boss Name");
-        public static readonly AttributeEnum PLAYER_JOB = new AttributeEnum(6, "Player Job");
-        public static readonly AttributeEnum PLAYER_NAME = new AttributeEnum(7, "Player Name");
-        public static readonly AttributeEnum DPS = new AttributeEnum(8, "DPS");
-        public static readonly AttributeEnum DAMAGE_PERCENTAGE = new AttributeEnum(9, "Damage Percentage");
-        public static readonly AttributeEnum MAX_HIT_INDIVIDUAL = new AttributeEnum(10, "Max Hit (Individual Player)");
-        public static readonly AttributeEnum HPS = new AttributeEnum(11, "HPS");
-        public static readonly AttributeEnum HEALING_PERCENTAGE = new AttributeEnum(12, "Healing Percentage");
-        public static readonly AttributeEnum MAX_HEAL = new AttributeEnum(13, "Max Heal");
-        public static readonly AttributeEnum OVERHEAL_PERCENTAGE = new AttributeEnum(14, "Overheal Percentage");
-        public static readonly AttributeEnum DEATHS = new AttributeEnum(15, "Deaths");
-        public static readonly AttributeEnum CRITS = new AttributeEnum(16, "Crits");
-        public static readonly AttributeEnum MISSES = new AttributeEnum(17, "Misses");
+        public static readonly AttributeEnum StartTime = new AttributeEnum("Start Time");
+        public static readonly AttributeEnum Duration = new AttributeEnum("Duration");
+        public static readonly AttributeEnum MaxHitParty = new AttributeEnum("Max Hit (Party)");
+        public static readonly AttributeEnum TotalHealing = new AttributeEnum("Total Healing");
+        public static readonly AttributeEnum MapName = new AttributeEnum("Map Name");
+        public static readonly AttributeEnum TargetName = new AttributeEnum("Target Name");
+        public static readonly AttributeEnum PlayerJob = new AttributeEnum("Player Job");
+        public static readonly AttributeEnum PlayerName = new AttributeEnum("Player Name");
+        public static readonly AttributeEnum DPS = new AttributeEnum("DPS");
+        public static readonly AttributeEnum DamagePerc = new AttributeEnum("Damage%");
+        public static readonly AttributeEnum MaxHitIndividual = new AttributeEnum("Max Hit (Individual)");
+        public static readonly AttributeEnum HPS = new AttributeEnum("HPS");
+        public static readonly AttributeEnum HealingPerc = new AttributeEnum("Healing%");
+        public static readonly AttributeEnum MaxHeal = new AttributeEnum("Max Heal");
+        public static readonly AttributeEnum OverHealPerc = new AttributeEnum("Overheal%");
+        public static readonly AttributeEnum Deaths = new AttributeEnum("Deaths");
+        public static readonly AttributeEnum Crits = new AttributeEnum("Crits");
+        public static readonly AttributeEnum CritDmgPerc = new AttributeEnum("Crit Dmg%");
+        public static readonly AttributeEnum CritHealPerc = new AttributeEnum("Crit Heal%");
+        public static readonly AttributeEnum Misses = new AttributeEnum("Misses");
 
-        public static IEnumerable<AttributeEnum> Values
+        public string Name { get; }
+
+        public AttributeEnum(string name)
         {
-            get
-            {
-                yield return START_TIME;
-                yield return DURATION;
-                yield return MAX_HIT_PARTY;
-                yield return TOTAL_HEALING;
-                yield return MAP_NAME;
-                yield return BOSS_NAME;
-                yield return PLAYER_JOB;
-                yield return PLAYER_NAME;
-                yield return DPS;
-                yield return DAMAGE_PERCENTAGE;
-                yield return MAX_HIT_INDIVIDUAL;
-                yield return HPS;
-                yield return HEALING_PERCENTAGE;
-                yield return MAX_HEAL;
-                yield return OVERHEAL_PERCENTAGE;
-                yield return DEATHS;
-                yield return CRITS;
-                yield return MISSES;
-            }
-        }
-
-        private readonly int index;
-        private readonly String name;
-
-        AttributeEnum(int index, String name)
-        {
-            this.index = index;
-            this.name = name;
-        }
-
-        public int Index { get { return this.index; } }
-
-        public String Name { get { return this.name; } }
-
-        public static AttributeEnum GetAttributeByIndex(int index)
-        {
-            foreach (AttributeEnum a in AttributeEnum.Values)
-            {
-                if (a.Index == index)
-                {
-                    return a;
-                }
-            }
-
-            return null;
+            this.Name = name;
         }
 
         public override string ToString()
         {
-            return this.name;
-        }
-
-        public override bool Equals(object obj)
-        {
-            var item = obj as AttributeEnum;
-
-            if (item == null)
-            {
-                return false;
-            }
-
-            return this.index.Equals(item.index);
-        }
-
-        public override int GetHashCode() { 
-            return this.index.GetHashCode();
+            return this.Name;
         }
     }
-
 }
---
+++ App/Enum/SuccessLevelEnum.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace MognetPlugin.Enum
+{
+    public class SuccessLevelEnum
+    {
+        public static readonly SuccessLevelEnum Clear = new SuccessLevelEnum(1, "Clear");
+        public static readonly SuccessLevelEnum RanAway = new SuccessLevelEnum(2, "Ran Away");
+        public static readonly SuccessLevelEnum Wipe = new SuccessLevelEnum(3, "Wipe");
+
+        public int Code { get; }
+        public string Name { get; }
+
+        SuccessLevelEnum(int code, string name)
+        {
+            this.Code = code;
+            this.Name = name;
+        }
+
+        public static IEnumerable<SuccessLevelEnum> Values
+        {
+            get
+            {
+                yield return Clear;
+                yield return RanAway;
+                yield return Wipe;
+            }
+        }
+
+        public static SuccessLevelEnum GetByCode(int code)
+        {
+            foreach (SuccessLevelEnum s in SuccessLevelEnum.Values)
+            {
+                if (s.Code == code)
+                {
+                    return s;
+                }
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as SuccessLevelEnum;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return this.Code.Equals(item.Code);
+        }
+
+        public override int GetHashCode() { 
+            return this.Code.GetHashCode();
+        }
+    }
+
+}
