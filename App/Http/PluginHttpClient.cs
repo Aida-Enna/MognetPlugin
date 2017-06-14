1038250885559b55a74c90a5aff7b93b63c4a2cc
Bruno Lima <castanho.bruno@gmail.com>
2017-06-14 06:22:02 -0300

    Add Http client

---
+++ App/Http/PluginHttpClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MognetPlugin.Http
+{
+    class PluginHttpClient
+    {
+        private string BaseURL = "https://mognet.herokuapp.com";
+        private HttpClient Client;
+
+        public PluginHttpClient()
+        {
+            Client = new HttpClient();
+        }
+
+        public HttpResponseMessage PostDiscord(string json, string token)
+        {
+            StringContent Body = new StringContent(json, Encoding.UTF8, "application/json");
+            this.Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", token);
+
+            return Client.PostAsync(new Uri(BaseURL + "/discord"), Body).Result;
+        }
+
+        public HttpResponseMessage Get(string token)
+        {
+            return Client.GetAsync(new Uri(BaseURL + "/check-token/" + token)).Result;
+        }
+    }
+}
